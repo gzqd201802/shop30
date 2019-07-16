fb942ae243e001de3f5fc9838f056a64f34a5c1d
Sujiehao <sujiehao@itcast.cn>
2019-07-16 17:16:06 +0800

    跳转页面的两种方式

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -156,7 +156,17 @@ Page({
       });
 
   },
-
+  // 点击商品，跳转到商品详情页
+  goToDetail(event) {
+    // 从事件对象中提取 商品 id
+    const {
+      id
+    } = event.currentTarget.dataset;
+    // 通过 wxAPI 跳转页面
+    wx.navigateTo({
+      url: '/pages/goods_detail/goods_detail?goods_id=' + id,
+    });
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
