d227a5c8ef8ea8764425abd17543c7011e16cf37
Sujiehao <sujiehao@itcast.cn>
2019-07-18 16:59:09 +0800

    微信小程序权限

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -10,6 +10,34 @@ Page({
   },
   // 点击选择收货地址
   chooseAddress() {
+    // 在调用收货地址功能前先 检查权限
+    wx.getSetting({
+      // 返回值中只会出现小程序已经向用户请求过的权限。
+      success: res => {
+        console.log(res);
+        // 获取授权信息
+        const addressAuth = res.authSetting['scope.address'];
+        // 如果检测权限的时候，发现收货地址的结果为 false ，打开设置引导用户开启权限
+        if (addressAuth === false) {
+          // 调起客户端小程序设置界面 - 引导用户开启权限
+          wx.openSetting({
+            // 从设置界面点击返回后就会触发 success 回调函数
+            success: result => {
+              // 每次关闭设置界面的时候，都尝试调用收货地址功能，开启权限的话自动弹出收货地址
+              this.chooseAddressMain();
+            }
+          })
+          // 如果用户权限列表中是已授权 true 或者 没有地址授权信息 undefined
+        } else if (addressAuth === true || addressAuth === undefined) {
+          // 调用收货地址功能
+          this.chooseAddressMain();
+        }
+      }
+    })
+
+  },
+  // 收货地址核心 功能封装
+  chooseAddressMain() {
     // 调用收货地址 api 
     wx.chooseAddress({
       success: res => {
