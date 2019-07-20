8eebf6f112ddab3e34efd506365774ec712da739
Sujiehao <sujiehao@itcast.cn>
2019-07-20 17:56:13 +0800

    授权登录完成

--- pages/auth/auth.js
+++ pages/auth/auth.js
@@ -1,66 +1,64 @@
-// pages/auth/auth.js
+// 引入之前封装的 request 方法
+const {
+  request
+} = require("../../utils/request.js");
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+  // 1. 获取用户信息按钮的事件
+  getUserInfo(event) {
+    // 1.1 解构用户登录需要用到的四个参数
+    const {
+      encryptedData,
+      iv,
+      rawData,
+      signature
+    } = event.detail;
 
+    // 2. 继续获取登录的 code
+    wx.login({
+      success: res => {
+        // 2.1 获取 code
+        const {
+          code
+        } = res;
+
+        // 3. 调用登录接口获取 token
+        // 需要传入 5 个必要参数
+        this.getTokenData({
+          encryptedData,
+          iv,
+          rawData,
+          signature,
+          code
+        });
+      }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  // 3.0 封装登录接口获取 token
+  getTokenData(params) {
+    // 调用登录接口
+    request({
+      url: 'users/wxlogin',
+      method: 'POST',
+      data: {
+        ...params
+      }
+    }).then(res => {
+      // 防止 res 返回值为 null，程序报错
+      if (res) {
+        // 4. 解构返回值的 token
+        const {
+          token
+        } = res;
+        // 4.1 把 token 保存到本地
+        wx.setStorageSync('token', token);
+        // 4.2 返回上一页
+        wx.navigateBack();
+      }
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    })
   }
+
 })
